d5a3e41ef822618875aa0e28668762c6ce58ce57
Edwinwkuria <edwin.w.kuria@gmail.com>
2019-11-20 15:38:01 +0300

    Creating a windows Forms Application

--- Payroll/Form1.cs
+++ Payroll/Form1.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Payroll
 {
-    public partial class adminLandingPage : Form
+    public partial class Form1 : Form
     {
-        public adminLandingPage()
+        public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- Payroll/Properties/AssemblyInfo.cs
+++ Payroll/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b726d5c6-b69c-4136-b582-dd3ba3bcc1be")]
+[assembly: Guid("b6312031-a13a-4cc0-8c01-53c0aa5e75e7")]
 
 // Version information for an assembly consists of the following four values:
 //
